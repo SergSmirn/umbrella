3f9ba0f3ca935ed5e0221e7eabfc5b185d04bc09
Smirnov & Maksimov <kurchatov1996@yandex.ru>
2016-10-05 22:18:17 +0300

    Hot fix

--- src/ray2d.hpp
+++ src/ray2d.hpp
@@ -4,101 +4,103 @@
 #include <cmath>
 #include <iostream>
 
-class Ray2d
+class Ray2D
 {
 public:
-  Ray2d() = default;
+  Ray2D() = default;
 
-  Ray2d(Ray2d const & obj):
-  Origin(obj.Origin), Direction(obj.Direction)
+  Ray2D(Ray2D const & obj):
+  m_origin(obj.m_origin), m_direction(obj.m_direction)
   {}
 
-  Ray2d(Point2D Orig, Point2D Dir)
-  :Origin(Orig), Direction (Dir)
+  Ray2D(Point2D Orig, Point2D Dir)
+  :m_origin(Orig), m_direction (Dir)
   {
 
   }
   //Принадлежность точки четверти  в которой лежит луч
-  bool region (Point2D Point)
+  bool Region (Point2D Point)
   {
 
-      if ((((Direction.x()-Origin.x())/(Point.x()-Origin.x()))>=0)
-       && (((Direction.y()-Origin.y())/(Point.y()-Origin.y()))>=0)) return 1;
+      if ((((m_direction.x()-m_origin.x())/(Point.x()-m_origin.x()))>=0)
+       && (((m_direction.y()-m_origin.y())/(Point.y()-m_origin.y()))>=0)) return 1;
       return 0;;
   }
   //Проверка пересечения с прям-ом
-  bool 	crossing (Box2d const & obj)
+  bool 	Crossing (Box2D const & obj)
   {
-      factorA=Origin.y()-Direction.y(),
-      factorB=Direction.x()- Direction.x(),
-      factorC=Origin.x()*Direction.y()-Direction.x()*Origin.y();
+      factorA=m_origin.y()-m_direction.y(),
+      factorB=m_direction.x()- m_direction.x(),
+      factorC=m_origin.x()*m_direction.y()-m_direction.x()*m_origin.y();
 
       return ((obj.LeftBot().y()<=(-factorA*obj.LeftBot().x()-factorC)/factorB && obj.RightTop().y()>=(-factorA*obj.LeftBot().x()-factorC)/factorB
           || obj.LeftBot().y()<=(-factorA*obj.RightTop().x()-factorC)/factorB && obj.RightTop().y()>=(-factorA*obj.RightTop().x()-factorC)/factorB)
           || (obj.LeftBot().x()<=(-factorB*obj.LeftBot().y()-factorC)/factorA && obj.RightTop().x()>=(-factorB*obj.LeftBot().y()-factorC)/factorA
           || obj.LeftBot().x()<=(-factorB*obj.RightTop().y()-factorC)/factorA && obj.RightTop().x()>=(-factorB*obj.RightTop().y()-factorC)/factorA))
-          && (region(obj.LeftBot())||region(obj.RightTop()));
+          && (Region(obj.LeftBot())||Region(obj.RightTop()));
   }
 
 
-  Ray2d(std::initializer_list<Point2D> const & lst)
+  Ray2D(std::initializer_list<Point2D> const & lst)
   {
-    Point2D * vals[] = { &Origin, &Direction};
+    Point2D * vals[] = { &m_origin, &m_direction};
     int const count = sizeof(vals) / sizeof(vals[0]);
     auto it = lst.begin();
     for (int i = 0; i < count && it != lst.end(); i++, ++it)
       *vals[i] = *it;
   }
 
-  Point2D & OriginN () { return Origin;}
-  Point2D & DirectionN () { return Direction;}
+  Point2D & OriginN () { return m_origin;}
+  Point2D & DirectionN () { return m_direction;}
 
-  Point2D const & OriginN () const { return Origin;}
-  Point2D const & DirectionN () const { return Direction;}
+  Point2D const & OriginN () const { return m_origin;}
+  Point2D const & DirectionN () const { return m_direction;}
 
   float & A () { return factorA;}
   float & B () { return factorB;}
   float & C () { return factorC;}
 
-  bool operator == (Ray2d const & obj) const
+  bool operator == (Ray2D const & obj) const
   {
-    return Origin==obj.OriginN() && Direction==obj.DirectionN();
+    return m_origin==obj.OriginN() && m_direction==obj.DirectionN();
   }
 
-  Ray2d moveX (float X)
+  Ray2D MoveX (float x)
   {
-    Point2D distance(X,0);
-    Origin+=distance;
-    Direction+=distance;
+    Point2D distance(x,0);
+    m_origin+=distance;
+    m_direction+=distance;
     return *this;
   }
 
-  Ray2d moveY (float Y)
+  Ray2D MoveY (float y)
   {
-    Point2D distance(0,Y);
-    Origin+=distance;
-    Direction+=distance;
+    Point2D distance(0,y);
+    m_origin+=distance;
+    m_direction+=distance;
     return *this;
   }
 
-  Ray2d move (Point2D distance)
+  Ray2D Move (Point2D distance)
   {
-    Origin+=distance;
-    Direction+=distance;
+    m_origin+=distance;
+    m_direction+=distance;
     return *this;
   }
+
+private:
 
   bool EqualWithEps(float v1, float v2) const
   {
     return fabs(v1 - v2) < kEps;
   }
-private:
-  Point2D Origin{0.0f,0.0f}, Direction{0.0f,0.0f};
+
+  Point2D m_origin{0.0f,0.0f}, m_direction{0.0f,0.0f};
   float factorA=0.0f,factorB=0.0f,factorC=0.0f;
 
 };
 
-std::ostream & operator << (std::ostream & os, Ray2d const & obj)
+std::ostream & operator << (std::ostream & os, Ray2D const & obj)
 {
   os << "Ray2D {" << obj.OriginN() << ", " << obj.DirectionN() << "}";
   return os;
