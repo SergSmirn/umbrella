37fe507b1ea13e9990d86d33ba0c86eaae436a2a
Smirnov & Maksimov <kurchatov1996@yandex.ru>
2016-11-15 12:51:40 +0300

    Fix codestyle

--- tests/test2.cpp
+++ tests/test2.cpp
@@ -58,34 +58,34 @@ TEST(factory_test, construction)
 {
   Factory factory;
   auto bullet = factory.Create<Bullet>(Point2D(1.0f, 1.0f), Point2D(1.1f, 1.1f), 15);
-  EXPECT_EQ(bullet->GetName(),"Bullet");
-  EXPECT_EQ(bullet->GetDamage(),15);
-  EXPECT_EQ(bullet->LeftBot(),Point2D(1.0f, 1.0f));
-  EXPECT_EQ(bullet->RightTop(),Point2D(1.1f, 1.1f));
+  EXPECT_EQ(bullet->GetName(), "Bullet");
+  EXPECT_EQ(bullet->GetDamage(), 15);
+  EXPECT_EQ(bullet->LeftBot(), Point2D(1.0f, 1.0f));
+  EXPECT_EQ(bullet->RightTop(), Point2D(1.1f, 1.1f));
 
   auto bulletManager = factory.Create<BulletManager>();
-  EXPECT_EQ(bulletManager->GetBullets().empty(),true);
+  EXPECT_EQ(bulletManager->GetBullets().empty(), true);
 
   auto alien1 = factory.Create<Alien>(Point2D(1.0f, 1.0f), Point2D(2.0f, 2.0f), 90, 5, 15, 2);
-  EXPECT_EQ(alien1->GetName(),"Alien");
-  EXPECT_EQ(alien1->LeftBot(),Point2D(1.0f, 1.0f));
-  EXPECT_EQ(alien1->RightTop(),Point2D(2.0f, 2.0f));
-  EXPECT_EQ(alien1->GetHealth(),90);
-  EXPECT_EQ(alien1->GetRate(),5);
-  EXPECT_EQ(alien1->GetSpeed(),15);
-  EXPECT_EQ(alien1->GetType(),2);
+  EXPECT_EQ(alien1->GetName(), "Alien");
+  EXPECT_EQ(alien1->LeftBot(), Point2D(1.0f, 1.0f));
+  EXPECT_EQ(alien1->RightTop(), Point2D(2.0f, 2.0f));
+  EXPECT_EQ(alien1->GetHealth(), 90);
+  EXPECT_EQ(alien1->GetRate(), 5);
+  EXPECT_EQ(alien1->GetSpeed(), 15);
+  EXPECT_EQ(alien1->GetType(), 2);
 
   auto gun1 = factory.Create<Gun>(Point2D(1.0f, 1.0f), Point2D(3.0f, 3.0f), 100, 5, 10);
-  EXPECT_EQ(gun1->GetName(),"Gun");
-  EXPECT_EQ(gun1->LeftBot(),Point2D(1.0f, 1.0f));
-  EXPECT_EQ(gun1->RightTop(),Point2D(3.0f, 3.0f));
-  EXPECT_EQ(gun1->GetHealth(),100);
-  EXPECT_EQ(gun1->GetRate(),5);
-  EXPECT_EQ(gun1->GetSpeed(),10);
+  EXPECT_EQ(gun1->GetName(), "Gun");
+  EXPECT_EQ(gun1->LeftBot(), Point2D(1.0f, 1.0f));
+  EXPECT_EQ(gun1->RightTop(), Point2D(3.0f, 3.0f));
+  EXPECT_EQ(gun1->GetHealth(), 100);
+  EXPECT_EQ(gun1->GetRate(), 5);
+  EXPECT_EQ(gun1->GetSpeed(), 10);
 
   auto obstacle1 = factory.Create<Obstacle>(Point2D(1.0f, 1.0f), Point2D(3.0f, 3.0f), 45);
-  EXPECT_EQ(obstacle1->GetName(),"Obstacle");
-  EXPECT_EQ(obstacle1->LeftBot(),Point2D(1.0f, 1.0f));
-  EXPECT_EQ(obstacle1->RightTop(),Point2D(3.0f, 3.0f));
-  EXPECT_EQ(obstacle1->GetStrength(),45);
+  EXPECT_EQ(obstacle1->GetName(), "Obstacle");
+  EXPECT_EQ(obstacle1->LeftBot(), Point2D(1.0f, 1.0f));
+  EXPECT_EQ(obstacle1->RightTop(), Point2D(3.0f, 3.0f));
+  EXPECT_EQ(obstacle1->GetStrength(), 45);
 }
